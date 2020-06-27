80f10268382280ece21227ebb3969be728a27f22
Ajith Anand <ajithanand303@gmail.com>
2020-06-27 22:39:11 +0530

    basic structure,menu bar

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,24 @@
-print("Ohh shit..Here we go again")
+from tkinter import *
+
+root = Tk()
+
+
+class Calculator:
+    def __init__(self, master):
+        # title and size
+        master.title("Calculator")
+        master.geometry("500x500")
+        # menu bar
+        menubar = Menu(master)
+        list=Menu(menubar,tearoff=0)
+        list.add_command(label="history")
+        menubar.add_cascade(label="Options",menu=list)
+        master.config(menu=menubar)
+        #display field
+        result = StringVar()
+        input_field = Entry(root, textvariable=result)
+        input_field.place(width=500, height=50)
+
+
+my_calc = Calculator(root)
+root.mainloop()
---
+++ sample_menu.py
@@ -0,0 +1,19 @@
+from tkinter import Toplevel, Button, Tk, Menu
+
+top = Tk()
+menubar = Menu(top)
+file = Menu(menubar, tearoff=0)
+file.add_command(label="New")
+file.add_command(label="Open")
+file.add_command(label="Save")
+file.add_command(label="Save as...")
+file.add_command(label="Close")
+
+file.add_separator()
+
+file.add_command(label="Exit", command=top.quit)
+
+menubar.add_cascade(label="File", menu=file)
+
+top.config(menu=menubar)
+top.mainloop()
