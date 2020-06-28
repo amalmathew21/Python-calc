3fbf918783f9835bf509b65caa591183a94add12
Ajith Anand <ajithanand303@gmail.com>
2020-06-28 18:51:10 +0530

    history window added

--- Calculator.py
+++ Calculator.py
@@ -4,6 +4,15 @@ root = Tk()
 
 
 class Calculator:
+    def hist(self):
+        self.f=open("history_calc.txt","r")
+        self.temp=Tk()
+        self.temp.title("history")
+        self.temp.geometry("100x300")
+        self.t=Text(self.temp,height=300,width=100)
+        self.t.pack()
+        self.t.insert(END,self.f.read())
+
     def __init__(self, master):
         # title and size
         master.title("Calculator")
@@ -11,7 +20,7 @@ class Calculator:
         # menu bar
         menubar = Menu(master)
         list=Menu(menubar,tearoff=0)
-        list.add_command(label="history")
+        list.add_command(label="history",command=self.hist)
         list.add_command(label="Exit",command=master.quit)
         menubar.add_cascade(label="Options",menu=list)
         master.config(menu=menubar)
